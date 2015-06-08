e7a1abcd29863324753befd8b57f15b4ff42808d
zaq111 <sin1983@yahoo.com.sg>
2015-06-09 06:23:22 +0700

    Update angelimus-ssh-dropbear.sh

--- angelimus-ssh-dropbear.sh
+++ angelimus-ssh-dropbear.sh
@@ -2,7 +2,7 @@
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -ivh epel-release-6-8.noarch.rpm
 sudo yum update && sudo yum install dropbear
-sudo service dropbear start
 sudo chkconfig dropbear on
 clear
+sudo service dropbear start
 echo "angelimus.blogspot.com" 
