e7d4a5794555f1d2a208db12113cd202ba40eb94
zaq111 <sin1983@yahoo.com.sg>
2015-06-21 08:09:47 +0700

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,8 @@
 wget http://cdn.primatelabs.com/Geekbench-3.3.2-Linux.tar.gz
 tar -xzf Geekbench-3.3.2-Linux.tar.gz
+sleep 3
 cd dist
+sleep 2
 cd Geekbench-3.3.2-Linux
-./bench
+sleep 1
+./geekbench
