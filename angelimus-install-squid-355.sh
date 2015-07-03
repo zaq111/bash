0c80a2a4fae46eabed26fc16e6f61d69e8775df9
zaq111 <sin1983@yahoo.com.sg>
2015-07-04 06:31:31 +0700

    Update angelimus-install-squid-355.sh

--- angelimus-install-squid-355.sh
+++ angelimus-install-squid-355.sh
@@ -1,12 +1,4 @@
 cd /etc/yum.repos.d/
-echo -e "
-[squid]
-name=Squid beta repo for CentOS Linux 6 - $basearch
-#IL mirror
-baseurl=http://www1.ngtech.co.il/rpm/centos/6/$basearch
-failovermethod=priority
-enabled=1
-gpgcheck=0" >> angelimus.repo
-sleep 1
-yum-config-manager --enable squid
-yum -y update
+wget https://raw.githubusercontent.com/zaq111/bash/master/angelimus-repo-squid355.repo
+yum-config-manager --enable angelimus_squid
+yum update
