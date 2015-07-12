eeb5ab737b778f90984c9e380c7334e2131f6a45
zaq111 <sin1983@yahoo.com.sg>
2015-07-12 09:10:59 +0700

    Update angelimus-install-vesta.sh

--- angelimus-install-vesta.sh
+++ angelimus-install-vesta.sh
@@ -5,9 +5,18 @@ echo "Welcome to angelimus.com installer"
 echo " "
 echo "This will install VestaCP into your VPS"
 echo " "
+echo "Read This Carefully "
+echo "1. This Will Uninstall your apache first, or"
+echo "   httpd service"
+echo "2. This Will disable Selinux."
+echo "3. Follow instruction when installing. "
 echo "*********************************************"
 
 read -p $'Press any key to continue...\n' -n1 key
+clear
+echo "Removing Apache / httpd..."
+sleep 2
+yum -y remove httpd
 clear
 echo "Starting process..."
 sleep 2
